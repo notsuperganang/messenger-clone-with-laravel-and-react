2aad8325f6799c28106e953bcabfc33944f904eb
Ganang Setyo Hadi <ganang@mhs.usk.ac.id>
2024-04-27 06:40:31 +0700

    Generate Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Group;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Ganang Setyo Hadi',
+            'email' => 'ganangsetyohadi@gmail.com',
+            'password' => bcrypt('kantong123'),
+            'is_admin' => true,
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Farhanaul Khair',
+            'email' => 'farhankhair58@gmail.com',
+            'password' => bcrypt('password'),
         ]);
+
+        User::factory(10)->create();
+
+        for ($i = 0; $i < 5; $i++) {
+            $group = Group::factory()->create([
+                'owner_id' => 1,
+            ]);
+
+            $users = User::inRandomOrder()->limit(rand(2, 5))->pluck('id');
+            $group->users()->attach(array_unique([1, ...$users]));
+        }
     }
 }
